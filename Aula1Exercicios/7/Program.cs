1e1e124b1f510679af655f4dc4be8d9646cfc59b
Meuniki <42386616+Meuniki@users.noreply.github.com>
2019-09-13 14:34:39 -0300

    atividades

---
+++ Aula1Exercicios/7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pax;
+            double total;
+
+            Console.Write("Quntos passageiros? : ");
+            pax = int.Parse(Console.ReadLine());
+
+            total = pax* 4.20;
+            Console.WriteLine($"Você arrecadou R${total}");
+        }
+    }
+}
