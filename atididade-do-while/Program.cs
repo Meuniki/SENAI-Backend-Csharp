1d37a75882aefe13ef4bdadfcd04beff3a6a25a4
Meuniki <42386616+Meuniki@users.noreply.github.com>
2019-09-19 12:23:33 -0300

    exercicio-01-do-while

---
+++ atididade-do-while/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace atididade_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, cont = 1, acum = 0;
+
+            do{
+
+                cont++;
+                Console.Write($"Digite um número:");
+                num = int.Parse(Console.ReadLine());
+
+                if(num %2 != 0){
+                    acum += num;
+                }
+
+            }while(cont<=20);
+            Console.WriteLine($"A soma de todos impares é: {acum}");
+        }
+    }
+}
