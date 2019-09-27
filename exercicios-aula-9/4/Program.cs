73c3d30162d04f6c79b9f67b32b4a2ed77861bbe
Meuniki <42386616+Meuniki@users.noreply.github.com>
2019-09-27 15:55:07 -0300

    exercicios de fic

---
+++ exercicios-aula-9/4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criar um método do tipo double onde retorna o maior número de um vetor de
+            // tamanho indeterminado.
+
+
+        }
+
+        static double
+    }
+}
